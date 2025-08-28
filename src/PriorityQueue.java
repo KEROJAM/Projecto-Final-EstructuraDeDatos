2d3c45b3f4ed2eb26b960b690bb8aac2f8bcbaac
Alejandro Garcia Pelayo <alejandrogarciapelayo@gmail.com>
2025-08-27 21:46:10 -0600

    Update PriorityQueue.java

--- src/PriorityQueue.java
+++ src/PriorityQueue.java
@@ -1,53 +1,87 @@
-public class PriorityQueue{
-
-    PriorityNode[] data ; 
+public class PriorityQueue {
+    PriorityNode[] data;
     int size;
-
+    int capacity;
 
     public PriorityQueue() {
-      data = null;
-      size = 10;
+        capacity = 10;
+        data = new PriorityNode[capacity + 1]; // Index 0 is unused
+        size = 0;
     }
 
-    public void pop(){
-
+    public boolean isEmpty() {
+        return size == 0;
     }
 
-    public void push(int priority, String value){
+    public void push(int priority, String value) {
+        if (size >= capacity) {
+            resize();
+        }
 
-        //creamos  nodo a insertar
         PriorityNode nodeToInsert = new PriorityNode(priority, value);
-        //aumentamos el tamaño de la cola priorizada
+        size++;
+        data[size] = nodeToInsert;
 
-        this.size++;
-
-        //si es el  primero lo ponemos en la posición 1 y listo
-
-        if (size ==1){
-
-            data[size] = nodeToInsert;
-
-        } else {
-            //Si no  está vacía la cola priorizada
-            //la  posición temporal es la última
-            int myPosition = size;
-            //ponemos  el nodo ahí
-            data[myPosition] = nodeToInsert;
-            //encontramos  la posición del padre
-            int myParentPosition =(int)(myPosition/2);
-            //Y  guardamos el valor del padre
-            PriorityNode myParent=  data[myParentPosition];
-            //repetimos  intercambiar el padre con el hijo hasta que el padre
-
-            //tenga  mayor prioridad que el hijo
-
-            while (myPosition!=1 &&  myParent.getPriority() > nodeToInsert.getPriority()){
-                data[myPosition] = myParent;
-                data[myParentPosition] =  nodeToInsert;
-                myPosition =myParentPosition;
-                myParentPosition = (int)(myParentPosition/2);
-                myParent= data[myParentPosition];
+        // Heapify up
+        int currentPosition = size;
+        while (currentPosition > 1) {
+            int parentPosition = currentPosition / 2;
+            if (data[parentPosition].getPriority() > data[currentPosition].getPriority()) {
+                swap(parentPosition, currentPosition);
+                currentPosition = parentPosition;
+            } else {
+                break;
             }
         }
+    }
+
+    public PriorityNode pop() {
+        if (isEmpty()) {
+            return null;
+        }
+
+        PriorityNode result = data[1];
+        data[1] = data[size];
+        data[size] = null;
+        size--;
+
+        // Heapify down
+        int currentPosition = 1;
+        while (currentPosition * 2 <= size) {
+            int leftChild = currentPosition * 2;
+            int rightChild = currentPosition * 2 + 1;
+            int smallestChild = leftChild;
+
+            if (rightChild <= size && data[rightChild].getPriority() < data[leftChild].getPriority()) {
+                smallestChild = rightChild;
+            }
+
+            if (data[currentPosition].getPriority() > data[smallestChild].getPriority()) {
+                swap(currentPosition, smallestChild);
+                currentPosition = smallestChild;
+            } else {
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    private void swap(int i, int j) {
+        PriorityNode temp = data[i];
+        data[i] = data[j];
+        data[j] = temp;
+    }
+
+    private void resize() {
+        capacity *= 2;
+        PriorityNode[] newData = new PriorityNode[capacity + 1];
+        System.arraycopy(data, 0, newData, 0, data.length);
+        data = newData;
+    }
+
+    public void clear() {
+        data = new PriorityNode[capacity + 1];
+        size = 0;
     }
 }
